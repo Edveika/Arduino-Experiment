0101488bba741ea2b79e6917769eb483e1ce8bfa
Edveika <edvinasbureika@gmail.com>
2024-02-04 19:12:24 +0200

    Single blink indentation fix

--- SingleBlink/SingleBlink.ino
+++ SingleBlink/SingleBlink.ino
@@ -20,8 +20,8 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-    digitalWrite(2, HIGH);
-    delay(1000);
-    digitalWrite(2, LOW);
-    delay(1000);
+  digitalWrite(2, HIGH);
+  delay(1000);
+  digitalWrite(2, LOW);
+  delay(1000);
 }
